c255453b35c8d9747d5f579b8c1de86472ec9b9c
Luis Tupa <luis.tupa@orbis.com.pe>
2020-11-26 12:52:37 -0500

    feat: add initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example NextJS
+
+This example seeks to understand the functioning of NextJS.
---
+++ pages/home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrapper } from '../../views/home';
+
+const Home: React.FC = () => (
+  <Wrapper>Home</Wrapper>
+);
+
+export default Home;
